670389333bac154a10f52d8737a112def2ec2841
tenghaoxiang <873030913@qq.com>
2019-04-26 09:32:49 +0800

    mysql逆向工程生成domain类

---
+++ src/main/java/top/haibaraai/wx_login_pay/domain/Chapter.java
@@ -0,0 +1,57 @@
+package top.haibaraai.wx_login_pay.domain;
+
+
+public class Chapter {
+
+  private Integer id;
+  private Integer videoId;
+  private String title;
+  private Integer ordered;
+  private java.util.Date createTime;
+
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+
+  public Integer getVideoId() {
+    return videoId;
+  }
+
+  public void setVideoId(Integer videoId) {
+    this.videoId = videoId;
+  }
+
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+
+  public Integer getOrdered() {
+    return ordered;
+  }
+
+  public void setOrdered(Integer ordered) {
+    this.ordered = ordered;
+  }
+
+
+  public java.util.Date getCreateTime() {
+    return createTime;
+  }
+
+  public void setCreateTime(java.util.Date createTime) {
+    this.createTime = createTime;
+  }
+
+}
