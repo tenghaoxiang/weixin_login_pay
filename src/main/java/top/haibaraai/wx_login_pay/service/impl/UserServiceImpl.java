21d3733719ac037a63a45d36466fdb6cc944bbba
tenghaoxiang <873030913@qq.com>
2019-05-02 22:44:59 +0800

    完整获取用户信息

--- src/main/java/top/haibaraai/wx_login_pay/service/impl/UserServiceImpl.java
+++ src/main/java/top/haibaraai/wx_login_pay/service/impl/UserServiceImpl.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import top.haibaraai.wx_login_pay.config.WechatConfig;
 import top.haibaraai.wx_login_pay.domain.User;
+import top.haibaraai.wx_login_pay.mapper.UserMapper;
 import top.haibaraai.wx_login_pay.service.UserService;
 import top.haibaraai.wx_login_pay.utils.HttpUtils;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Date;
 import java.util.Map;
 
 @Service
@@ -15,6 +17,9 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private WechatConfig wechatConfig;
+
+    @Autowired
+    private UserMapper userMapper;
 
     @Override
     public User saveWechatUser(String code) {
@@ -35,6 +40,15 @@ public class UserServiceImpl implements UserService {
          * 获得授权用户唯一标识
          */
         String openid = (String) map.get("openid");
+
+        User dbUser = userMapper.findByOpenId(openid);
+
+        if (dbUser != null) {
+            /**
+             * 已经有用户信息，直接返回
+             */
+            return dbUser;
+        }
 
         if (accessToken != null && openid != null) {
 
@@ -60,9 +74,20 @@ public class UserServiceImpl implements UserService {
                 e.printStackTrace();
             }
 
+            /**
+             * 创建一个user保存用户信息
+             */
             String address = country + "||" + province + "||" + city;
-            System.out.println(address);
+            User user = new User();
+            user.setOpenid(openid);
+            user.setName(nickname);
+            user.setSex(sex);
+            user.setCity(address);
+            user.setHeadImg(headImg);
+            user.setCreateTime(new Date());
 
+            userMapper.save(user);
+            return user;
 
         }
 
