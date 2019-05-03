911a138b0391e599de3cfe3f9b79bc089b2037be
tenghaoxiang <873030913@qq.com>
2019-05-03 10:53:05 +0800

    扫码登录模块完成

--- src/main/java/top/haibaraai/wx_login_pay/controller/loginController.java
+++ src/main/java/top/haibaraai/wx_login_pay/controller/loginController.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import top.haibaraai.wx_login_pay.config.WechatConfig;
 import top.haibaraai.wx_login_pay.domain.JsonData;
+import top.haibaraai.wx_login_pay.domain.User;
 import top.haibaraai.wx_login_pay.service.UserService;
+import top.haibaraai.wx_login_pay.utils.JwtUtils;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
@@ -40,8 +43,15 @@ public class loginController {
     @RequestMapping("/user/callback")
     public void wechatUserCallback(@RequestParam(value = "code") String code,
                                    @RequestParam(value = "state") String state,
-                                   HttpServletResponse response) {
-        userService.saveWechatUser(code);
+                                   HttpServletResponse response) throws IOException {
+        User user = userService.saveWechatUser(code);
+        if (user != null) {
+            //生成jwt
+            String token = JwtUtils.geneJsonWebToken(user);
+            //state 当前用户的页面地址，需要拼接http://，这样才不会站内跳转
+            response.sendRedirect(state + "?token=" + token + "&head_img=" + user.getHeadImg() + "&name=" + user.getName());
+        }
+
     }
 
 }
